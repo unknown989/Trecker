4f57102ad8dad83ecbfd799b7b2fba32de081f9c
unknown989 <unknown989@protonmail.com>
2022-04-18 17:20:38 +0000

    hide navbar with user is not logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,12 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
+import isAuth from "../../hooks/isAuth";
 import "./Navbar.css";
 
 function Navbar() {
   let location = useLocation();
+  let [logged, islogged] = isAuth();
   const [homeActive, setHomeActive] = useState(false);
   const [meActive, setMeActive] = useState(false);
   const [logoutActive, setLogoutActive] = useState(false);
@@ -40,28 +42,31 @@ function Navbar() {
         break;
       }
     }
-    
   }, [location]);
-  return (
-    <div className="navbar-container">
-      <Link className="link-icon" to={"/"}>
-        <div className={`navbar-elem ${homeActive ? "activated" : ""}`}>
-          <IconHome />
-        </div>
-      </Link>
+  if (logged) {
+    return (
+      <div className="navbar-container">
+        <Link className="link-icon" to={"/"}>
+          <div className={`navbar-elem ${homeActive ? "activated" : ""}`}>
+            <IconHome />
+          </div>
+        </Link>
 
-      <Link className="link-icon" to={"/me"}>
-        <div className={`navbar-elem ${meActive ? "activated" : ""}`}>
-          <IconUser />
-        </div>
-      </Link>
-      <Link className="link-icon" to={"/logout"}>
-        <div className={`navbar-elem ${logoutActive ? "activated" : ""}`}>
-          <IconLogout />
-        </div>
-      </Link>
-    </div>
-  );
+        <Link className="link-icon" to={"/me"}>
+          <div className={`navbar-elem ${meActive ? "activated" : ""}`}>
+            <IconUser />
+          </div>
+        </Link>
+        <Link className="link-icon" to={"/logout"}>
+          <div className={`navbar-elem ${logoutActive ? "activated" : ""}`}>
+            <IconLogout />
+          </div>
+        </Link>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
 
 export default Navbar;
